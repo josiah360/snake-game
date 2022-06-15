a989b5a386b0887f2b37b65458a91e6fb80414cc
Josiah Olajide <josiaholajide@gmail.com>
2022-06-15 15:49:09 +0100

    html and css done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './style.css';
+
+
+const body = document.querySelector('body');
+const div = document.createElement('div');
+div.id = 'game-board'
+body.appendChild(div)
+
+div.innerHTML = '';
+
+console.log(div)
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|svg|png|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'Snake',
+        })
+    ],
+
+}
