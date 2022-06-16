cfd582a24176f5fd97000affd2bf563afe3d56cd
Josiah Olajide <josiaholajide@gmail.com>
2022-06-16 01:10:57 +0100

    Implement game loop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './style.css'
+
 const body = document.querySelector('body');
 const gameBoard = document.createElement('div');
 gameBoard.id = 'game-board';
@@ -6,3 +8,17 @@ body.appendChild(gameBoard)
 
 // -------------------------------------------------
 
+let lastRenderTime = 0;
+const gameSpeed = 2;
+
+function main(currentTime) {
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if(secondsSinceLastRender < 1/gameSpeed) return
+
+    
+    lastRenderTime = currentTime
+    console.log('render')
+}
+
+window.requestAnimationFrame(main)
