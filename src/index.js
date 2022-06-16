a34a65aebb0d183e13b62b1aaa6fd97e58a7521d
Josiah Olajide <josiaholajide@gmail.com>
2022-06-16 18:10:00 +0100

    Start Over

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css'
-import { snakeSpeed, update as updateSnake, draw as drawSnake } from './snake';
+import { snakeSpeed, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from './snake';
 import { update as updateFood, draw as drawFood } from './food';
-
+import { outsideGrid } from './grid';
 
 const body = document.querySelector('body');
 const gameBoard = document.createElement('div');
@@ -11,29 +11,3 @@ body.appendChild(gameBoard);
 
 //------------------------------------------
 
-let lastRenderTime = 0;
-
-function main(currentTime) {
-    window.requestAnimationFrame(main);
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if(secondsSinceLastRender < 1/snakeSpeed) return
-
-    lastRenderTime = currentTime;
-    console.log('render')
-
-    update()
-    draw()
-}
-
-window.requestAnimationFrame(main);
-
-function update() {
-    updateFood()
-    updateSnake()
-}
-
-function draw() {
-    gameBoard.innerHTML = '';
-    drawFood(gameBoard)
-    drawSnake(gameBoard)
-}
