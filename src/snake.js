2944443e7d636a3b1a7ec3f14d61d75ca40472c9
Josiah Olajide <josiaholajide@gmail.com>
2022-06-17 17:57:17 +0100

    snake.js done

---
+++ src/snake.js
@@ -0,0 +1,24 @@
+export const SNAKE_SPEED = 2;
+const snakeBody = [
+    {x: 11, y: 11},
+    {x: 12, y: 11},
+    {x: 13, y: 11}
+];
+
+export function update() {
+    for(let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = {...snakeBody[i]}
+    }
+    snakeBody[0].x += 0
+    snakeBody[0].y += 0
+}
+
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div');
+        snakeElement.classList.add('snake');
+        snakeElement.style.gridColumnStart = segment.x;  
+        snakeElement.style.gridRowStart = segment.y ;
+        gameBoard.appendChild(snakeElement);
+    })
+}
