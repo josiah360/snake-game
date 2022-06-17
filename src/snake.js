cc2d07d2ebd2e69542be23398ef3df884e309a49
Josiah Olajide <josiaholajide@gmail.com>
2022-06-18 00:00:05 +0100

    input.js done

--- src/snake.js
+++ src/snake.js
@@ -1,5 +1,6 @@
+import { getInputDirection } from "./input";
 
-export const SNAKE_SPEED = 1;
+export const SNAKE_SPEED = 5;
 const snakeBody = [
     {x: 11, y: 11},
     {x: 12, y: 11},
@@ -7,12 +8,13 @@ const snakeBody = [
 ]
 
 export function update() {
+    const inputDirection = getInputDirection()
     for(let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = {...snakeBody[i]}
     }
 
-    snakeBody[0].x += 1;
-    snakeBody[0].y += 0;
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
