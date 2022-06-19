7857144c44a43a484c475c8fed48afd3692d91c2
Josiah Olajide <josiaholajide@gmail.com>
2022-06-19 16:43:21 +0100

    HTML done

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const body = document.querySelector('body');
+const gameBoard = document.createElement('div');
+gameBoard.id = 'game-board';
+body.appendChild(gameBoard);
+
+//--------------------------------------------------
