963b8c18c3885ab6faa8011505b9111ccba0c88d
Josiah Olajide <josiaholajide@gmail.com>
2022-06-19 16:54:01 +0100

    CSS done

--- src/index.js
+++ src/index.js
@@ -6,3 +6,30 @@ gameBoard.id = 'game-board';
 body.appendChild(gameBoard);
 
 //--------------------------------------------------
+
+let lastRenderTime = 0;
+const SNAKE_SPEED = 2;
+
+function main(currentTime) {
+    window.requestAnimationFrame(main)
+
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    lastRenderTime = currentTime
+    console.log('render')
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+
+}
+
+function draw() {
+    
+}
